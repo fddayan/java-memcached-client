a307c12a68fd9373a1474b2ca48b57873e35b454
Alexander Sokolovsky <alexander.sokolovsky.a@gmail.com>
2010-11-15 17:13:20 +0200

    Bugfixing not_my_vbucket occurs on optimized set
    Setting correct vbucket index to the optimized set operation
    Added warn message if a not_my_vbucket occurs
    
    Change-Id: I0a1699e88f3ed6ec3d0dc8524ffb1d1c1853a85b

--- src/main/java/net/spy/memcached/protocol/BaseOperationImpl.java
+++ src/main/java/net/spy/memcached/protocol/BaseOperationImpl.java
@@ -155,6 +155,10 @@ public abstract class BaseOperationImpl extends SpyObject implements Operation {
 		handlingNode = to;
 	}
 
+	public int getVbucket() {
+		return vbucket;
+	}
+
 	public void setVBucket(int vbucket) {
 		this.vbucket = vbucket;
 	}
